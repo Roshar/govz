cd8e3221788542a699070c6468c3cac480d9009f
Roshah <rashid.batukaev@gmail.com>
2018-12-24 23:13:04 +0300

    create jquery slider

--- js/mado.js
+++ js/mado.js
@@ -1,47 +1,43 @@
 $(document).ready(function(){
+	// var imageCollection = ['uploads/slider/11.jpg','uploads/slider/22.jpg',
+	// 'uploads/slider/33.jpg','uploads/slider/44.jpg','uploads/slider/55.jpg',
+	// 'uploads/slider/66.jpg','uploads/slider/77.jpg'];
+
 	var imageCollection = ['uploads/slider/11.jpg','uploads/slider/22.jpg',
-	'uploads/slider/33.jpg','uploads/slider/44.jpg','uploads/slider/55.jpg',
-	'uploads/slider/66.jpg','uploads/slider/77.jpg'];
+	'uploads/slider/33.jpg'];
 
-	var current = 1;
+	
 	var index = 0;
-	var indexdot = 1;
 
-	for (var i = 1; i <= imageCollection.length; i++) {
-		if (i == 1) {
-			$('.dot-content').append('<div class="dot active"></div>');	
-		}
+	// $('.dot-content').has('span')
+	// .append('<div class="dot active"></div>')
+
+	for (var i = 0; i < imageCollection.length; i++) {
 		$('.dot-content').append('<div class="dot"></div>');
+		if (i == 0) {
+			$('.dot').addClass('active')
+		}
 	}
-
-	var dots = document.getElementsByClassName('dot');
 
 	setInterval(function(){
 
-	 var image = $('.slide-main-content');
+		var dots = document.getElementsByClassName('dot');
 
-	 for (var i = index; i < imageCollection.length; i++) {
-	 	if(indexdot >= 1) {
-	 		dots[indexdot-1].classList.remove("active");
-	 	}
-	 	if (indexdot > imageCollection.length) {
-	 		dots[indexdot-1].classList.remove("active");
-	 	}
-	 	dots[indexdot].classList.add("active");
-	 }
-    
-	 image.css({
-      "background-image": "url("+imageCollection[index]+")"});
+		for(var i = index; i < dots.length; i++) {
+			$('.dot').eq(index).addClass('active');
+			$('.dot').eq(index-1).removeClass('active');
+		}
 
-		index++;
-		indexdot++;
 
-	if(index>=imageCollection.length){
-	    index = 0;
-	}
-	if (indexdot > imageCollection.length) {
-	 		indexdot = 1;
-	 	}
-	},4000);
+		$('.slide-main-content').css({
+		'background-image':'url('+imageCollection[index]+')'
+		});
+		console.log(index)
+		index ++;
+		if (index >= imageCollection.length) {
+			index = 0;
+		}
+	},2000)
 
+	
 });
