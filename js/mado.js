413c5c9616c30155f4de12ff0ced2c3b73f07bb1
Rashid <rashid.batukaev@gmail.com>
2018-12-24 16:56:36 +0300

    finish version2

--- js/mado.js
+++ js/mado.js
@@ -5,15 +5,43 @@ $(document).ready(function(){
 
 	var current = 1;
 	var index = 0;
+	var indexdot = 1;
+
+	for (var i = 1; i <= imageCollection.length; i++) {
+		if (i == 1) {
+			$('.dot-content').append('<div class="dot active"></div>');	
+		}
+		$('.dot-content').append('<div class="dot"></div>');
+	}
+
+	var dots = document.getElementsByClassName('dot');
+
 	setInterval(function(){
-	 var image = $('slide-main-content');
-	 $('.slide-main-content').css('background-image',
-			'url('+imageCollection[index])+')'
-	 index++;
+
+	 var image = $('.slide-main-content');
+
+	 for (var i = index; i < imageCollection.length; i++) {
+	 	if(indexdot >= 1) {
+	 		dots[indexdot-1].classList.remove("active");
+	 	}
+	 	if (indexdot > imageCollection.length) {
+	 		dots[indexdot-1].classList.remove("active");
+	 	}
+	 	dots[indexdot].classList.add("active");
+	 }
+    
+	 image.css({
+      "background-image": "url("+imageCollection[index]+")"});
+
+		index++;
+		indexdot++;
+
 	if(index>=imageCollection.length){
 	    index = 0;
 	}
+	if (indexdot > imageCollection.length) {
+	 		indexdot = 1;
+	 	}
 	},4000);
-	
 
 });
