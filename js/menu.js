c79b25a8e29d9d4c097bb4a0825e55a6315a4340
Rashid <rashid.batukaev@gmail.com>
2018-12-27 14:51:15 +0300

    last version adaptive menu

--- js/menu.js
+++ js/menu.js
@@ -1,10 +1,5 @@
 $(document).ready(function(){
-	$(function() {
-		$('.dropdown').hover(function(){
-			$(this).children('.dropdown-menu').stop(false,true).fadeIn(300)
-		},function(){
-			$(this).children('.dropdown-menu').stop(false,true).fadeOut(300)
-		})
+	$('.dropdown').hover(function(){
+		$(this).children('ul').stop(true, false, true).fadeToggle(400);
 	})
-
 })
